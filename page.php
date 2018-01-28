97dbce4584274686d11d6e217bc3e80fcf979293
Donato Abiuso <donato.abiuso@gmail.com>
2018-01-28 21:35:08 +0100

    A Propos - Section 3

--- page.php
+++ page.php
@@ -49,6 +49,35 @@
     </section>
     <!-- section close -->
 
+    <!-- TROISIEME SECTION -->
+    <!-- section begin -->
+    <section id="section-team" class="bg-grey">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="text-center">
+                        <h2><?php the_field('propossect3_titre') ?></h2>
+                        <div class="tiny-border"></div>
+                    </div>
+                </div>
+                <?php while (have_rows('propossect3_repetear')):the_row(); ?>
+                <div class="col-md-3">
+                    <div class="team-box">
+                        <div class="team-inner">
+                            <img src="<?php the_sub_field('image_propos_section3') ?>" alt="" class="img-circle">
+                            <div class="mask"></div>
+                        </div>
+                        <h6><?php the_sub_field('texte_propos_section3') ?></h6>
+                        <div class="subtext"><?php the_sub_field('fonction_propos_section3') ?></div>
+                    </div>
+                </div>
+                <?php endwhile;?>
+            </div>
+        </div>
+    </section>
+    <!-- section close -->
 
+</div>
+<!-- content close -->
 
 <?php get_footer(); ?>
