f2cf4af5706cbd0ad98771d086bd25250b619792
Donato Abiuso <donato.abiuso@gmail.com>
2018-01-28 22:04:34 +0100

    Article - Section 3

--- single.php
+++ single.php
@@ -28,6 +28,37 @@
                         <div class="post-media">
                             <img src="<?php the_field('articleImage') ?>" class="img-responsive">
                         </div>
+<!-- TROISIEME SECTION -->
+                        <?php while (have_posts()):the_post() ?>
+                            <div class="post-content">
+                                <div class="post-title">
+                                    <h1><?php the_title() ?></h1>
+                                </div>
+                                <div class="post-metadata">
+                                            <span class="posted-on">
+                                                <i class="fa fa-clock-o"></i>
+                                                <?php echo get_the_date(); ?>
+                                            </span>
+                                    <span class="byline">
+                                                <i class="fa fa-user"></i>
+                                                <a href="<?php the_permalink(); ?>"><?php the_author(); ?></a>
+                                            </span>
+                                    <span class="cat-links">
+                                                <i class="fa fa-folder-open"></i>
+                                                <a href="<?php the_permalink(); ?>"><?php the_category('-') ?></a>
+                                            </span>
+                                </div>
+                                <div class="hr"></div>
+                                <div class="post-entry">
+                                    <p><?php the_content() ?></p>
+                                <div class="clearfix"></div>
+                                </div>
+                            </div>
+                        <?php endwhile; ?>
+                    </article>
+                    <!-- post close -->
+                </div>
+
 
 
 <?php get_footer(); ?>
